989056a76d4f0305df33d7dab9b2ae7f940828f3
Naman Sharma <namansharma882006@gmail.com>
2025-10-12 20:00:41 +0530

    Project Completed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { tr } from "framer-motion/m";
   export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api":"/api",
     withCredentials:true,
